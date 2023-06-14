7cb89972b64023a804481b8204c4b3167fa4ffc1
MahbuburRahmanRasel <inspironkhl@gmail.com>
2023-06-14 23:19:06 +0600

    add teacher

--- src/Pages/Dashboard/MyClass.jsx
+++ src/Pages/Dashboard/MyClass.jsx
@@ -15,7 +15,39 @@ const MyClass = () => {
 
     return (
         <div>
-            <p>{myclass.length}</p>
+            <p className='text-2xl font-bold text-center'> Total Posted Class : {myclass.length}</p>
+            <div className=" overflow-y-scroll overflow-x-scroll h-96">
+          <table className="table mx-auto w-2/3">
+            <thead className="bg-theme-1 text-theme-3-100 text-sm">
+              <tr>
+                <th></th>
+               
+                <th>Class Name</th>
+                <th>Enrolled Sutdents</th>
+                <th>Status</th>
+                <th>Feedback</th>
+                
+              </tr>
+            </thead>
+            <tbody>
+            {
+              myclass.map((item,index)=>(
+                  <tr key={index}>
+                <th>{index + 1}</th>
+                <td>{item.languageName}</td>
+                <td>{item.totalStudents}</td>
+                <td>{item.status}</td>
+                <td>{item?.feedback}</td>
+                
+              </tr>
+  
+              ))
+            }
+              
+            </tbody>
+          </table>
+        </div>
+
         </div>
     );
 };
